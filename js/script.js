0c72aa57aef838a0cdf41408f6d986976c8fc389
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-28 12:43:34 +0200

    added an img

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Ciao sono un h1'
+            message: 'Ciao sono un h1',
+            src: 'https://images.mubicdn.net/images/film/188918/cache-284496-1511897314/image-w1280.jpg?size=800x',
         }
     }
 }).mount('#app')
