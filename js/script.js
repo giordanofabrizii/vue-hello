f9bb7f61e420e7b7a56fb0bcbf4b54f1fa01629d
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-28 13:18:06 +0200

    fixed the classes switch

--- js/script.js
+++ js/script.js
@@ -5,16 +5,15 @@ createApp({
         return {
             message: 'CALIMERO DANCE',
             src: 'https://images.mubicdn.net/images/film/188918/cache-284496-1511897314/image-w1280.jpg?size=800x',
+            imgClasses: '',
+            titleClasses: '',
         }
     },
     methods: {
         graySwitch: function (){
-            let img = document.querySelector("img");
-            img.classList.toggle("gray");
+            this.imgClasses = this.imgClasses == 'gray' ? '' : 'gray';
 
-
-            let title = document.querySelector("h1");
-            title.classList.toggle("rainbow")
+            this.titleClasses = this.titleClasses == 'rainbow' ? '' : 'rainbow';
         }
     }
 }).mount('#app');
