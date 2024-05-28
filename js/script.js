f4a68d4568c419264e049db7073b35adefd9bfa1
giordanofabrizii <giordanofabrizi@gmail.com>
2024-05-28 12:50:05 +0200

    added an action on the click of the img

--- js/script.js
+++ js/script.js
@@ -6,5 +6,11 @@ createApp({
             message: 'Ciao sono un h1',
             src: 'https://images.mubicdn.net/images/film/188918/cache-284496-1511897314/image-w1280.jpg?size=800x',
         }
+    },
+    methods: {
+        graySwitch: function (){
+            let img = document.querySelector("img");
+            img.classList.toggle("gray")
+        }
     }
 }).mount('#app')
